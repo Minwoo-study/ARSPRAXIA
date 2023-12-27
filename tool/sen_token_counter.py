9d972ea0adbe972add4e5ad8bb6256c34c9c9f54
Siadel <60665481+Siadel@users.noreply.github.com>
2023-12-27 14:26:49 +0900

    sen_token_count 업데이트

--- tool/sen_token_counter.py
+++ tool/sen_token_counter.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 import json, re
 
+from tool.common import ConsoleLogger
 
 # target_dir = Path("C:\\Users\\정진혁\\Ars Praxia\\[T] 23-05-001 NIA AI 데이터셋 구축 - General\\06-수집데이터\\가공 완료 데이터\\가공 완료 데이터 종합(바로잡음)")
 
-def main(target_folder:Path, result_file_path:str|Path, *, tag_count:bool=False, tag_by_PubType:bool=False):
+def main(target_folder:Path, result_file_path:str|Path, *,report_interval:int=10000, tag_count:bool=False, tag_by_PubType:bool=False):
+
+    cl = ConsoleLogger()
 
     result = {
         "files":0,
@@ -55,6 +58,15 @@ def main(target_folder:Path, result_file_path:str|Path, *, tag_count:bool=False,
                         result["tags"][pubType][tag] = 1
                     else:
                         result["tags"][pubType][tag] += 1
+        
+        if result["files"] % report_interval == 0:
+            cl.print(f"{result['files']} files processed.")
+    
+    if tag_count and not tag_by_PubType:
+        result["tags"] = dict(sorted(result["tags"].items(), key=lambda x:x[1], reverse=True))
+    elif tag_count and tag_by_PubType:
+        for pubType in result["tags"]:
+            result["tags"][pubType] = dict(sorted(result["tags"][pubType].items(), key=lambda x:x[1], reverse=True))
 
     with open(result_file_path, "w", encoding="utf-8") as f:
         json.dump(result, f, indent=2, ensure_ascii=False)
